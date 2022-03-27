e2b733f0308ffaab97eb7da2161bace0d8d04c49
Priyansh Thakar <priyanshthakar@gmail.com>
2022-03-27 18:28:21 -0400

    Fixed errors in apiResponse.json

--- src/main/java/com/example/assignment02gc200472583/ApiResponse.java
+++ src/main/java/com/example/assignment02gc200472583/ApiResponse.java
@@ -1,44 +1,9 @@
 package com.example.assignment02gc200472583;
 
 public class ApiResponse {
-    private int id;
-    private String firstName ;
-    private String lastName;
-    private String fullName;
-    private String title;
-    private String family;
-    private String image;
-    private String imageUrl;
+    private Character[] characters;
 
-    public int getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getFamily() {
-        return family;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
+    public Character[] getCharacters() {
+        return characters;
     }
 }
---
+++ src/main/java/com/example/assignment02gc200472583/Character.java
@@ -0,0 +1,44 @@
+package com.example.assignment02gc200472583;
+
+public class Character {
+    private int id;
+    private String firstName ;
+    private String lastName;
+    private String fullName;
+    private String title;
+    private String family;
+    private String image;
+    private String imageUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
